7e00d41db42beff3306c3d9c0a7a5ab215f156ce
akseryanin <sasha.seryanin@yandex.ru>
2020-05-06 00:03:06 +0300

    Init struct

---
+++ MindBoxSquareTask/MindBoxSquareTask/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MindBoxSquareTask
+{
+    public class Point
+    {
+        
+    }
+}
---
+++ MindBoxSquareTask/UnitTestMindbox/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UnitTestMindbox
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
